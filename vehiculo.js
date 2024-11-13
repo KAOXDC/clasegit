b7ce3a3c13f93bf80eb967ebe61ab40be01f67f8
Diego Prado <kaoxdc@gmail.com>
2024-11-13 07:42:10 -0500

    creacion de vehiculo js en rama1

---
+++ vehiculo.js
@@ -0,0 +1,9 @@
+class Vehiculo{
+    constructor (this, marca, cilindraje){
+        this.marca = marca 
+        this.cilindraje = cilindraje 
+    }
+}
+
+v1 = new Vehiculo('Kia', 1600)
+v2 = new Vehiculo('Mustang', 2800)
